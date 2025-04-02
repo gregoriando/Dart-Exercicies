e66f08e921a727607c69ec5a4e82d7699f31ed54
Gregory <gregorystp@icloud.com>
2025-04-02 20:16:16 -0300

    feat "Exercicio de contagem de caracteres feito"

---
+++ dart_contagem_de_caracteres/bin/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  String Character = "Gregory";
+  int count = 0;
+  for (int i = 0; i <= Character.length - 1; i++) {
+    count++;
+  }
+
+  print("O número de caracteres na String é: $count");
+}
--- dart_contagem_de_caracteres/test/dart_contagem_de_caracteres_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_contagem_de_caracteres/dart_contagem_de_caracteres.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
