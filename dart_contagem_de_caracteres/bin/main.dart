efc84676cbc8604d75a309bfa9b2f80ba32451a5
Gregory <gregorystp@icloud.com>
2025-04-03 20:00:05 -0300

    refactor "Ajustado o método para atender a performance do print"

--- dart_contagem_de_caracteres/bin/main.dart
+++ dart_contagem_de_caracteres/bin/main.dart
@@ -3,13 +3,15 @@ import 'dart:io';
 void main() {
   print("Digite uma palavra para contar os caracteres");
   String character = stdin.readLineSync()!.toLowerCase();
-  charactercounter(character);
+  print(
+    "O número de caracteres da palavra $character é: ${charactercounter(character)}",
+  );
 }
 
-void charactercounter(String character) {
+int charactercounter(String character) {
   int count = 0;
   for (int i = 0; i <= character.length - 1; i++) {
     count++;
   }
-  print("O número de caracteres da palavra $character é: $count");
+  return count;
 }
