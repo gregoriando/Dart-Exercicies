46762e8ee3fd57604a625934998423585ee7cedd
Gregory <gregorystp@icloud.com>
2025-04-07 20:25:28 -0300

    Fix "nos ifs eu tinha usado a base ao inves do expoente"

--- dart_potencia_recursiva/bin/main.dart
+++ dart_potencia_recursiva/bin/main.dart
@@ -3,11 +3,8 @@ void main() {
 }
 
 int power(int base, int expoent) {
-  if (base == 0) return 1;
-  if (base == 1) return base;
+  if (expoent == 0) return 1;
+  if (expoent == 1) return base;
 
   return base * power(base, expoent - 1);
 }
-
-// Importante mencionar a titulo de estudos que uma função recursiva funciona exatamente de um modo que ela encontra algo para parar e depois volta.
-// exemplo do base ele vai até o if (base == 1) return base; e depois retorna para conseguir fazer as contas , mas primeiro ele sempre usará esse retorno até o minimo.
