24128b84934ba23b2f1f2ddc15424d9b6ade251c
Gregory <gregorystp@icloud.com>
2025-04-28 20:28:26 -0300

    refactor "Feito a extension de lista em uma lib diferente para ser usado com a Main"

--- dart_insert_sort/bin/main.dart
+++ dart_insert_sort/bin/main.dart
@@ -1,19 +1,6 @@
+import 'package:insert_sort/list_extension.dart';
+
 void main() {
   List<int> numbers = [5, 2, 9, 1, 7, 6];
-  insertionSort(numbers);
-
-  print(numbers);
-}
-
-insertionSort(List<int> numbers) {
-  for (int i = 1; i < numbers.length; i++) {
-    int actual = numbers[i];
-    int j = i - 1;
-
-    while (j >= 0 && numbers[j] > actual) {
-      numbers[j + 1] = numbers[j];
-      j--;
-    }
-    numbers[j + 1] = actual;
-  }
+  print(numbers.insertionSort);
 }
