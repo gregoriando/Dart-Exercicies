946b89179aa795ba29fc6b8715c23298fcdf05da
Gregory <gregorystp@icloud.com>
2025-04-28 19:38:11 -0300

    refatoração "Feito a extensão da lista binária ao invés de usar uma funçÃo"

--- dart_lista_binaria/bin/main.dart
+++ dart_lista_binaria/bin/main.dart
@@ -1,24 +1,24 @@
 void main() {
   List<int> binary = [1, 3, 5, 7, 9, 11];
-  int number = 7;
-
-  int result = binarySearch(binary, number);
+  int result = binary.binarySearch(7);
   print("O indice do número procurado é : $result");
 }
 
-int binarySearch(List<int> binary, int number) {
-  int start = 0;
-  int end = binary.length - 1;
+extension IntExtension on List<int> {
+  int binarySearch(int number) {
+    int start = 0;
+    int end = length - 1;
 
-  while (start <= end) {
-    int middle = (start + end) ~/ 2;
-    if (binary[middle] == number) {
-      return middle;
-    } else if (binary[middle] < number) {
-      start = middle + 1;
-    } else {
-      end = middle - 1;
+    while (start <= end) {
+      int middle = (start + end) ~/ 2;
+      if (this[middle] == number) {
+        return middle;
+      } else if (this[middle] < number) {
+        start = middle + 1;
+      } else {
+        end = middle - 1;
+      }
     }
+    return -1;
   }
-  return -1;
 }
