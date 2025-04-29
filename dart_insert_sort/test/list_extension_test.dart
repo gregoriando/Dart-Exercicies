11b1bfa9b58a3520faed7bc8b081d13277a5c215
Gregory <gregorystp@icloud.com>
2025-04-28 21:20:45 -0300

    feat "Adicionado o test para garantir segurança de software"

---
+++ dart_insert_sort/test/list_extension_test.dart
@@ -0,0 +1,32 @@
+import 'package:insert_sort/list_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Insertion Sort', () {
+    test('Ver se está ordendando recebendo uma Lista', () {
+      List<int> numbers = [5, 2, 9, 1, 7, 6];
+      numbers.insertionSort();
+      expect(numbers, equals([1, 2, 5, 6, 7, 9]));
+    });
+    test('Lista Vazia', () {
+      List<int> numbers = [];
+      numbers.insertionSort();
+      expect(numbers, equals([]));
+    });
+    test('Lista única', () {
+      List<int> numbers = [10];
+      numbers.insertionSort();
+      expect(numbers, equals([10]));
+    });
+    test('Lista decrescente', () {
+      List<int> numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+      numbers.insertionSort();
+      expect(numbers, equals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    });
+    test('Lista Duplicada', () {
+      List<int> numbers = [10, 10, 12, 13, 16, 15, 15, 14, 14, 11];
+      numbers.insertionSort();
+      expect(numbers, equals([10, 10, 11, 12, 13, 14, 14, 15, 15, 16]));
+    });
+  });
+}
