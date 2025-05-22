723a7c7595db802306ec2f3ee8455749a165e64b
Gregory <gregorystp@icloud.com>
2025-05-22 18:18:19 -0300

    feat ' Feito testes  e classes '

--- dart_check_credit_card/lib/check_credit_card.dart
+++ dart_check_credit_card/lib/check_credit_card.dart
@@ -1,1 +1,24 @@
-class CheckCreditCard {}
+class CheckCreditCard {
+  static bool validadeCreditCard(String creditCard) {
+    creditCard = creditCard.replaceAll(RegExp(r'[^0-9]'), '');
+    creditCard = creditCard.split('').reversed.join();
+
+    if (creditCard.length != 16 || RegExp(r'^(\d)\1*$').hasMatch(creditCard)) {
+      return false;
+    } else {
+      int validate = 0;
+      for (var i = 0; i < creditCard.length; i++) {
+        int digit = int.parse(creditCard[i]);
+
+        if (i % 2 != 0) {
+          digit = digit * 2;
+          if (digit > 9) {
+            digit = digit - 9;
+          }
+        }
+        validate += digit;
+      }
+      return validate % 10 == 0;
+    }
+  }
+}
--- dart_check_credit_card/test/check_credit_card_test.dart
+++ dart_check_credit_card/test/check_credit_card_test.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'package:dart_check_credit_card/check_credit_card.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Classe CheckCreditCard', () {
+    test('Verifica se o cartão de crédito tem 16 números', () {
+      String creditCard = '4539 1488 0343 646';
+      expect(CheckCreditCard.validadeCreditCard(creditCard), equals(false));
+    });
+    test('Verifica se todos os números são iguais', () {
+      String creditCard = '6666 6666 6666 6666';
+      expect(CheckCreditCard.validadeCreditCard(creditCard), equals(false));
+    });
+    test('Verifica se o cartão é válido', () {
+      String creditCard = '4539 1488 0343 6467';
+
+      expect(CheckCreditCard.validadeCreditCard(creditCard), equals(true));
+    });
+    test('Verifica se o cartão é inválido', () {
+      String creditCard = '4539 1488 0343 6468';
+      expect(CheckCreditCard.validadeCreditCard(creditCard), equals(false));
+    });
+  });
+}
