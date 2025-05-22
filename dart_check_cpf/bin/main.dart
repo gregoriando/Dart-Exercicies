2ea902946d89835ee5bbc404f9cba154474e20d3
Gregory <gregorystp@icloud.com>
2025-05-22 18:18:02 -0300

    feat ' Feito testes  e classes '

--- dart_check_cpf/bin/main.dart
+++ dart_check_cpf/bin/main.dart
@@ -1,9 +1,9 @@
 import 'package:dart_check_cpf/check_cpf.dart';
 
 void main() {
-  String cpf = '369.837.858-27';
+  String cpf = '715.583.830-69';
   String cpf2 = '111.111.111-11';
-  String cpf3 = '783.182.708-00';
+  String cpf3 = '291.800.750-10';
 
   print(CheckCpf.validateCpf(cpf));
   print(CheckCpf.validateCpf(cpf2));
