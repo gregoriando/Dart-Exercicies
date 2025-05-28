2dcd2c367566af57486b44d6ec72d03c0897d49b
Gregory <gregorystp@icloud.com>
2025-05-28 16:07:50 -0300

    refactor "Usado a nova versão do switch"

--- dart_conversor_de_temperatura/lib/temperature_converter.dart
+++ dart_conversor_de_temperatura/lib/temperature_converter.dart
@@ -7,18 +7,19 @@ class TemperatureConverter {
   TemperatureConverter({required this.temperature, required this.type});
 
   Map<String, double> convert() {
-    switch (type) {
-      case TemperatureType.celsius:
-        return {
-          'Fahrenheit': (temperature * 9 / 5) + 32,
-          'Kelvin': temperature + 273.15,
-        };
-      case TemperatureType.fahrenheit:
-        final celsius = (temperature - 32) * 5 / 9;
-        return {'Celsius': celsius, 'Kelvin': celsius + 273.15};
-      case TemperatureType.kelvin:
-        final celsius = temperature - 273.15;
-        return {'Celsius': celsius, 'Fahrenheit': (celsius * 9 / 5) + 32};
-    }
+    return switch (type) {
+      TemperatureType.celsius => {
+        'Fahrenheit': (temperature * 9 / 5) + 32,
+        'Kelvin': temperature + 273.15,
+      },
+      TemperatureType.fahrenheit => {
+        'Celsius': (temperature - 32) * 5 / 9,
+        'Kelvin': ((temperature - 32) * 5 / 9) + 273.15,
+      },
+      TemperatureType.kelvin => {
+        'Celsius': temperature - 273.15,
+        'Fahrenheit': ((temperature - 273.15) * 9 / 5) + 32,
+      },
+    };
   }
 }
